2f369f5e3e86dc9f232c5bf5c600533e9b2e435d
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-02-21 02:36:47 +0900

    24.02.21 한국 투어 tvOS Game, 호랑이 삽입 함.

---
+++ KoreaTourGame.tvOS/Dumpster.swift
@@ -0,0 +1,25 @@
+//
+//  Dumpster.swift
+//  AuroraSkater
+//
+//  Created by 이윤지 on 1/23/24.
+//
+
+import SpriteKit
+
+class Dumpster: Obstacle {
+    
+    convenience init(){
+        self.init(imageNamed: "호랑이장애물1")
+    }
+    
+    override func initPhysics() {
+        let frontCollider = SKPhysicsBody(rectangleOf: CGSizeMake(5, self.size.height), center: CGPointMake(-(self.size.width / 2), 0))
+        
+        let topCollider = SKPhysicsBody(rectangleOf: CGSizeMake(self.size.width * 0.80, 5), center: CGPointMake(0, self.size.height / 2 - 10))
+        
+        self.physicsBody = SKPhysicsBody(bodies: [frontCollider,topCollider])
+        
+        super.initPhysics()
+    }
+}
---
+++ KoreaTourGame.tvOS/SKNodeUpdate.swift
@@ -0,0 +1,14 @@
+//
+//  SKNodeUpdate.swift
+//  AuroraSkater
+//
+//  Created by 이윤지 on 1/23/24.
+//
+
+import SpriteKit
+
+extension SKNode {
+    @objc func update(){
+        
+    }
+}
